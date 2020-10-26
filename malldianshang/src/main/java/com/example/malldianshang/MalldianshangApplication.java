db41eaa4c541318136950180854a0eca3bb4b91b
ppp123499 <2867976715@qq.com>
2020-10-26 13:34:15 +0800

    初次提交

---
+++ malldianshang/src/main/java/com/example/malldianshang/MalldianshangApplication.java
@@ -0,0 +1,13 @@
+package com.example.malldianshang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalldianshangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalldianshangApplication.class, args);
+    }
+
+}
---
+++ malldianshang/src/test/java/com/example/malldianshang/MalldianshangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.malldianshang;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalldianshangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
