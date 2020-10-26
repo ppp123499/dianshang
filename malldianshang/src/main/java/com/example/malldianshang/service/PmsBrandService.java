fa5ee5f9be48ddabc2fdb54cc99b70f6eb9228ce
ppp123499 <2867976715@qq.com>
2020-10-26 14:40:58 +0800

    初次提交

---
+++ malldianshang/src/main/java/com/example/malldianshang/service/PmsBrandService.java
@@ -0,0 +1,15 @@
+package com.example.malldianshang.service;
+
+import com.example.malldianshang.mbg.model.PmsBrand;
+
+import java.util.List;
+
+public interface PmsBrandService {
+    List<PmsBrand> listAllBrand();
+    int createBrand(PmsBrand brand);
+    int updateBrand(Long id,PmsBrand brand);
+    int deleteBrand(Long id);
+    List<PmsBrand> listBrand(int pageNum, int pageSize);
+
+    PmsBrand getBrand(Long id);
+}
